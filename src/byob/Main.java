249953286b560218c2c420b70e4348281230bc8f
Alessandro Fazio <agil3b3ast@hotmail.it>
2016-07-29 15:17:26 +0200

    Adjusted screen size

--- src/byob/Main.java
+++ src/byob/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("view_main.fxml"));
         primaryStage.setTitle("BYOB");
-        primaryStage.setScene(new Scene(root, 1024, 768));
+        primaryStage.setScene(new Scene(root, 640, 400));
         primaryStage.show();
     }
 
