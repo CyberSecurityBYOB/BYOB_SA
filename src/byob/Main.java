2896a2f8154c6fd6e223d4afb89386d5bfb01b22
Alessandro Fazio <agil3b3ast@hotmail.it>
2016-07-29 15:12:13 +0200

    Added FXML Structure for Stand Alone Application

--- src/byob/Main.java
+++ src/byob/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package byob;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -10,12 +10,11 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("view_main.fxml"));
+        primaryStage.setTitle("BYOB");
+        primaryStage.setScene(new Scene(root, 1024, 768));
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
