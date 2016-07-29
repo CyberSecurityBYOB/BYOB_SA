91102a39ef97260dd0a82d51c22972c10d9381ad
Alessandro Fazio <agil3b3ast@hotmail.it>
2016-07-29 19:02:44 +0200

    Full Frequency toggle for fixed or random range

--- src/byob/FrequencyController.java
+++ src/byob/FrequencyController.java
@@ -5,13 +5,17 @@ import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.control.Slider;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.layout.GridPane;
 import javafx.scene.text.Text;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class FrequencyController implements Initializable {
+
     @FXML private Text textChangeFrequency;
     @FXML private Text textMinChangeFrequency;
     @FXML private Text textMaxChangeFrequency;
@@ -20,6 +24,10 @@ public class FrequencyController implements Initializable {
     @FXML private Slider sliderMinChangeFrequency;
     @FXML private Slider sliderMaxChangeFrequency;
 
+    @FXML private ToggleButton toggleFixedFrequency;
+
+    @FXML private GridPane minVariableFrequencyGridPane;
+    @FXML private GridPane maxVariableFrequencyGridPane;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -44,6 +52,41 @@ public class FrequencyController implements Initializable {
                 textMaxChangeFrequency.setText(String.format("%.0f", newValue));
             }
         });
+    }
+
+    @FXML
+    private void handleToggleFixedFrequency(ActionEvent event) {
+
+        if (toggleFixedFrequency.isSelected()) {
+
+            textChangeFrequency.setVisible(true);
+            sliderChangeFrequency.setVisible(true);
+            minVariableFrequencyGridPane.setVisible(false);
+            maxVariableFrequencyGridPane.setVisible(false);
+            sliderMinChangeFrequency.setVisible(false);
+            sliderMaxChangeFrequency.setVisible(false);
+            textChangeFrequency.setManaged(true);
+            sliderChangeFrequency.setManaged(true);
+            minVariableFrequencyGridPane.setManaged(false);
+            maxVariableFrequencyGridPane.setManaged(false);
+            sliderMinChangeFrequency.setManaged(false);
+            sliderMaxChangeFrequency.setManaged(false);
+        }
+        else{
+
+            textChangeFrequency.setVisible(false);
+            sliderChangeFrequency.setVisible(false);
+            minVariableFrequencyGridPane.setVisible(true);
+            maxVariableFrequencyGridPane.setVisible(true);
+            sliderMinChangeFrequency.setVisible(true);
+            sliderMaxChangeFrequency.setVisible(true);
+            textChangeFrequency.setManaged(false);
+            sliderChangeFrequency.setManaged(false);
+            minVariableFrequencyGridPane.setManaged(true);
+            maxVariableFrequencyGridPane.setManaged(true);
+            sliderMinChangeFrequency.setManaged(true);
+            sliderMaxChangeFrequency.setManaged(true);
+        }
     }
 
 }
