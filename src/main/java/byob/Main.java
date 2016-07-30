1a9ba4b451d3ed31b541037b80c37eaf4ba96537
Alessandro Fazio <agil3b3ast@hotmail.it>
2016-07-30 17:13:13 +0200

    Adjusted path with resources folder

--- src/main/java/byob/Main.java
+++ src/main/java/byob/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("views/view_main.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/view_main.fxml"));
         primaryStage.setTitle("BYOB");
         primaryStage.setScene(new Scene(root, 640, 400));
         primaryStage.show();
